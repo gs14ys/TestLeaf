34b1ca1001e27272047c753c34b2dd510e6d5d53
gs14ys <122199327+gs14ys@users.noreply.github.com>
2023-01-10 16:27:04 +0530

    Fixed 1st index issue

--- MaximalSubstring.java
+++ MaximalSubstring.java
@@ -2,32 +2,37 @@ package week1.day2;
 
 public class MaximalSubstring {
 
-	String maximalWordString = null;
-
 	public static void main(String[] args) {
-		String givenString = "I study in TestLeaf abc";
+		String givenString = "Indians study in FreeTime";
 		MaximalSubstring maximalString = new MaximalSubstring();
 		System.out.println("MaximalString is: " + maximalString.maximalSubstringForCheckingCount(givenString));
 	}
 
+	String finalValue = null;
+
 	public String maximalSubstringForCheckingCount(String givenString) {
-
 		String[] splitArray = givenString.split(" ");
-		for (int i = 0; i < splitArray.length - 1; i++) {
+		for (int i = 0; i < splitArray.length; i++) {
+			String maximalWordString = null;
 
-			for (int j = i + 1; j < splitArray.length; j++) {
+			for (int j = 0; j < splitArray.length; j++) {
 
-				if (splitArray[i].length() > splitArray[j].length()) {
+				if (splitArray[i].length() >= splitArray[j].length()) {
 					maximalWordString = splitArray[i];
 				} else {
 					System.out.println(splitArray[i] + " is not a maximal String");
+					maximalWordString = null;
 					break;
 				}
-				maximalWordString = splitArray[i];
+				System.out.println(maximalWordString + " innerLoop");
 			}
-
+			if (maximalWordString != null) {
+				finalValue = maximalWordString;
+				System.out.println(finalValue + " outerLoop");
+				break;
+			}
 		}
-		return maximalWordString;
+		return finalValue;
 
 	}
 }
