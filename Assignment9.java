c4f52fd6f9e862607aba96f0af60c851649a6f97
gs14ys <122199327+gs14ys@users.noreply.github.com>
2023-01-27 04:32:21 +0530

    Update Assignment9.java

--- Assignment9.java
+++ Assignment9.java
@@ -1,24 +1,41 @@
 package week2.day2;
 
-public class Assignment9 {
+public class Assignment10 {
+	String s = "loveleetcode";
 
-	public static int nonRepeating() {
-		String s = "leetcode";
-		char[] ch = s.toCharArray();
-		int length = ch.length;
-		for (int i = 0; i < length; i++) {
-			for (int j = i + 1; j < length - 1; j++) {
-				if (ch[i] == ch[j]) {
+	public int findUniqueChar() {
+		int indexValueForUniqueElement = -2;
+		char ch[] = this.s.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = i; j < ch.length; j++) {
+				if ((ch[i] == ch[j]) && (j == ch.length - 1)) {
+					System.out.println("cannot find unique char:" + "-1");
+					break;
+				} else if ((ch[i] == ch[j]) && (j != ch.length - 1) && (i != j)) {
+					System.out.println("working on finding a unique character");
+					break;
+				} else if ((ch[i] == ch[j]) && (j != ch.length - 1) && (i == j)) {
+					System.out.println("working on finding a unique character");
 					continue;
-				} else {
-
+				} else if ((ch[i] != ch[j]) && (j != ch.length - 1)) {
+					System.out.println("working on finding a unique character, eventhough this is not matching");
+				} else if ((ch[i] != ch[j]) && (j == ch.length - 1) && (i != j)) {
+					System.out.println("Aiyoo found it jee, unique char index is:" + i);
+					indexValueForUniqueElement = i;
+					break;
 				}
+
+			}
+			if (indexValueForUniqueElement != -2) {
+				break;
 			}
 		}
-		return 0;
+		return indexValueForUniqueElement;
 	}
 
 	public static void main(String[] args) {
-
+		Assignment10 assign = new Assignment10();
+		System.out.println(assign.findUniqueChar());
 	}
+
 }
